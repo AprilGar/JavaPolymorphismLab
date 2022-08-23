10b928147ac47124ddcf3519f2eaed71619edc83
April Garlington <april.garlington@hotmail.co.uk>
2022-08-23 21:06:18 +0100

    Attempted to overload a method

--- main/java/human/Teacher.java
+++ main/java/human/Teacher.java
@@ -3,6 +3,7 @@ package human;
 public class Teacher extends Human {
 
     private String subjectSpecialism;
+    private Student student;
 
     public Teacher(String name, String subjectSpecialism){
         super(name);
@@ -14,9 +15,13 @@ public class Teacher extends Human {
         return "Coffee is amazing!";
     }
 
+    //Overload canDrink method
+    public String canDrink(String message){
+       return String.format("%s has been ALOT today, I need ", Human student.getName() + message);
+    }
+
     public String canRemainCalm(){
         return "No problem, I'll go through it again...";
     }
-
 
 }
--- test/java/TeacherTest.java
+++ test/java/TeacherTest.java
@@ -1,3 +1,4 @@
+import human.Student;
 import human.Teacher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,16 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TeacherTest {
 
     private Teacher teacher;
+    private Student student;
 
     @BeforeEach
     public void setUp(){
         this.teacher = new Teacher("Miss Garlington", "Chemistry");
+        this.student = new Student("Charlie", 7, "Walk");
     }
 
     @Test
     public void canDrink(){
         String expected = "Coffee is amazing!";
         String actual = teacher.canDrink();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void canDrink(String wine){
+        int expected = "Charlie has been ALOT today, I need wine";
+        int actual = teacher.canDrink(String message);
         assertEquals(expected, actual);
     }
 
